36a7bf9c6874efa76a3977682a6ef8dfa80584d0
tosh541 <konosu.satoshi@gmail.com>
2021-03-07 14:16:16 +0900

    Splash screenを作成. アプリのアイコンの背景を変更

---
+++ app/src/main/java/com/example/coffeenote/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.coffeenote
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1500)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}

abb79563891deaaa0460d9d6012fdb2b7e9e46cd
Satoshi Konosu <satoshi@SatoshinoMac-mini.local>
2021-04-05 21:46:15 +0900

    細かい修正を加えた

--- app/src/main/java/com/example/coffeenote/Dialogs.kt
+++ app/src/main/java/com/example/coffeenote/Dialogs.kt
@@ -12,13 +12,11 @@ import java.util.*
 class DateDialog(private val onSelected: (String) -> Unit) :
         DialogFragment(), DatePickerDialog.OnDateSetListener {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // Use the current date as the default date in the picker
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-        // Create a new instance of DatePickerDialog and return it
         return DatePickerDialog(requireActivity(), this, year, month, day)
     }
 

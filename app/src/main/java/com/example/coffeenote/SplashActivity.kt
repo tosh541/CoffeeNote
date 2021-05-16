c668aed4b3f42d76e574dfe4b3126bc4ae706b34
Satoshi Konosu <konosu.satoshi@gmail.com>
2021-05-16 20:20:39 +0900

    アプリ起動時にrealmのデータを全て削除するコードを記述

--- app/src/main/java/com/example/coffeenote/SplashActivity.kt
+++ app/src/main/java/com/example/coffeenote/SplashActivity.kt
@@ -3,15 +3,24 @@ package com.example.coffeenote
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
+import io.realm.kotlin.where
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    private val realm = Realm.getDefaultInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val results = realm.where<CoffeeNote>().findAll()
+        realm.beginTransaction()
+        results.deleteAllFromRealm()
+        realm.commitTransaction()
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(1500)
